ad9104e83c429cc43e1e26366881db6f3af97c1e
santex <santex@grml.gchq.ml>
2015-04-16 00:38:29 +0200

    should be ok

--- script/init.sh
+++ script/init.sh
@@ -14,9 +14,4 @@ git clone https://github.com/santex/flower.git
 
 
 cd flower;
-ip=$(echo 127.0.0.1); 
-pwd=$(pwd);   
 
-nohup  perl script/perl_peer --ip $ip --filepath "$pwd/data" &
-
-tail nohup.out &
